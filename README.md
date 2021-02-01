cbc72531f563c810d96fbb5ae0a623c422ea2ac3
Noah Day <sheltielover432@gmail.com>
2021-02-01 12:05:05 -0500

    Volumes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Cpp_RayTrace_Tutorial
 
+Project Repo for the Ray Tracing in... Series. The following are currently implemented within the repo:
+
+[_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
 [_Ray Tracing: The Next Week_](https://raytracing.github.io/books/RayTracingTheNextWeek.html)
--- RayTraceDemo/main.cpp
+++ RayTraceDemo/main.cpp
@@ -13,6 +13,7 @@
 
 #include "aarect.h"
 #include "box.h"
+#include "constant_medium.h"
 #include "material.h"
 #include "memory.h"
 #include "moving_sphere.h"
@@ -177,6 +178,36 @@ hittable_list cornell_box()
 	return objects;
 }
 
+hittable_list cornell_smoke()
+{
+  hittable_list objects;
+
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(7, 7, 7));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+
+    objects.add(make_shared<constant_medium>(box1, 0.01, color(0,0,0)));
+    objects.add(make_shared<constant_medium>(box2, 0.01, color(1,1,1)));
+
+    return objects;
+}
+
 //Adapted from https://www.geeksforgeeks.org/how-to-create-a-command-line-progress-bar-in-c-c/
 void loadingBar(int current, int mult)
 {
@@ -273,7 +304,6 @@ int main()
 		lookat = point3(0, 2, 0);
 		vfov = 20.0;
 		break;
-	default:
 	case 6:
 		world = cornell_box();
 		aspect_ratio = 1.0;
@@ -284,6 +314,16 @@ int main()
 		lookat = point3(278, 278, 0);
 		vfov = 40.0;
 		break;
+	default:
+	case 7:
+            world = cornell_smoke();
+            aspect_ratio = 1.0;
+            image_width = 600;
+            samples_per_pixel = 200;
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278, 278, 0);
+            vfov = 40.0;
+            break;
 	}
 	const vec3 vup(0, 1, 0);
 	const auto dist_to_focus = 10.0;
